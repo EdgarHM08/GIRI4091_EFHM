75cbe56c8d9b8fc3b6603d95c7a2d606ec6904f4
EdgarHM08 <EdgarHM08>
2024-07-01 16:50:05 -0600

    Chicharronera

---
+++ ejercicios/chicharronera.py
@@ -0,0 +1,8 @@
+a = float (input ('Ingresar el valos de A'))
+b = float (input ('Ingresar el valor de B'))
+c = float (input ('Ingresar el valor de C'))
+
+x1 = (-b + -(b*b - 4*a*c )**0.5)/(2*a)
+x2 = (-b - -(b*b - 4*a*c )**0.5)/(2*a)
+
+print(x1, ' ', x2)
